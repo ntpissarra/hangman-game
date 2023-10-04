282ea19259553a7a27b4fafb36012b47398a5c61
ntpissarra <nacopissarra@gmail.com>
2023-10-04 16:14:44 -0300

    refactor: rename utils.go to game.go

--- main.go
+++ main.go
@@ -4,29 +4,29 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/ntpissarra/hangman/utils"
+	"github.com/ntpissarra/hangman/game"
 )
 
 func main() {
 	var input string
 
-	utils.ClearScreen()
-	utils.PrintIntro()
+	game.ClearScreen()
+	game.PrintIntro()
 
 	for {
 		var word string
 
-		utils.FirstQuestion()
+		game.FirstQuestion()
 		fmt.Scan(&input)
 		input = strings.ToLower(input)
 
 		if input == "y" {
-			utils.GetWord(&word)
-			utils.StartGame(word)
+			game.GetWord(&word)
+			game.StartGame(word)
 		} else if input == "n" {
 			break
 		} else {
-			utils.ClearScreen()
+			game.ClearScreen()
 		}
 	}
 
